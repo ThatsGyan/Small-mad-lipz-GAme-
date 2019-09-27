aab7e1c2398dfaf242dc727ceb8a77c31ad277c4
GYANENDRA PAL SINGH <46261508+ItsSweaty@users.noreply.github.com>
2019-09-28 01:15:28 +0530

    Add files via upload

---
+++ Small mad lipz GAme/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string color, PluralNoune, Celebrity;
+
+            Console.Write("Enter Color Name ");
+            color =  Console.ReadLine();
+
+            Console.Write("Enter PluralNoune ");
+            PluralNoune = Console.ReadLine();
+
+            Console.Write("Enter Celebrity Name ");
+            Celebrity = Console.ReadLine();
+
+
+
+            Console.WriteLine("Roses are " +color);
+            Console.WriteLine(PluralNoune+" are blue");
+            Console.WriteLine("I Love "+Celebrity);
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+
+        }
+    }
+}
